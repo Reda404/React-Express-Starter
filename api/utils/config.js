f414452af4a40bae1959fcba7749467f4a2195d6
Reda404 <julienmerlin.dev@gmail.com>
2020-12-03 08:38:28 +0100

    Add standard as dev dependencies, Add command lint in both ui and api, Format code with standard

--- api/utils/config.js
+++ api/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT
+const PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
@@ -9,5 +9,5 @@ if (process.env.NODE_ENV === 'test') {
 
 module.exports = {
   MONGODB_URI,
-  PORT,
+  PORT
 }
